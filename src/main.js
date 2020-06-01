9ec58a16a2fbd00ce610b67dbcc6dfeac649ed29
joenix <joenix@qq.com>
2020-06-01 11:03:29 +0800

    feat: new mode

--- src/main.js
+++ src/main.js
@@ -1,8 +1,46 @@
-// Import Scaff
-import Scaff from "./scaff";
+// Service Worker
+import "./registerServiceWorker";
 
-// Import App
-import App from "./App.vue";
+// Export Useage
+export default (
+  // App
+  App,
 
-// Init App
-Scaff(App);
+  // Subscript Face
+  { subscribe },
+
+  // Runner
+  next
+) => {
+  subscribe("util", config => {
+    console.log("UTIL MODULE : ", config);
+  });
+
+  subscribe("route", config => {
+    console.log("ROUTE CONFIGURE : ", config);
+
+    config.mode = "history";
+    config.baseRoute = process.env.publicPath;
+  });
+
+  subscribe("component", (config, model) => {
+    config.prefix = "x";
+  });
+
+  subscribe("request", ({ data, headers }) => {
+    data.token = "wechat: kazami500";
+  });
+
+  subscribe("response", config => {
+    console.log("HTTP Response : ", config);
+  });
+
+  subscribe("custom", config => {
+    return {
+      author: "joenix"
+    };
+  });
+
+  // Runner
+  return next(App);
+};
